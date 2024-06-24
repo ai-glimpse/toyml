6b61c3c47c7077f69369d52a03024914a2c8431d
Xiangzhuang Shen <datahonor@gmail.com>
2024-06-24 22:45:43 +0800

    improve vector

--- toyml/types/vector.py
+++ toyml/types/vector.py
@@ -12,9 +12,14 @@ class Vector:
         >>> v = Vector([1, 2, 3])
         >>> print(v)
         Vector[1, 2, 3]
+        >>> for i in v:
+        ...     print(i)
+        1
+        2
+        3
     """
 
-    data: list[float]
+    _data: list[float]
 
     # def mean(self) -> float:
     #     pass
@@ -23,24 +28,27 @@ class Vector:
     #     pass
 
     def __repr__(self) -> str:
-        return f"Vector{self.data}"
+        return f"Vector{self._data}"
 
     def __len__(self) -> int:
-        return len(self.data)
+        return len(self._data)
 
     def __add__(self, other: Vector) -> Vector:
         self.__check_length(other)
-        return Vector([self.data[i] + other.data[i] for i in range(len(self.data))])
+        return Vector([self._data[i] + other._data[i] for i in range(len(self._data))])
 
     def __sub__(self, other: Vector) -> Vector:
         self.__check_length(other)
-        return Vector([self.data[i] - other.data[i] for i in range(len(self.data))])
+        return Vector([self._data[i] - other._data[i] for i in range(len(self._data))])
 
     def __getitem__(self, index: int) -> float:
-        return self.data[index]
+        return self._data[index]
 
     def __iter__(self):
-        return iter(self.data)
+        return iter(self._data)
 
-    def __check_length(self, other: Vector) -> bool:
-        return len(self.data) == len(other.data)
+    def __check_length(self, other: Vector) -> None:
+        if len(self._data) != len(other._data):
+            raise ValueError(
+                f"Length of vectors does not match: " f"one is {len(self._data)}, other is {len(other._data)}"
+            )
