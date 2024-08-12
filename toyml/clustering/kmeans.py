5979e6ad4d59bdbbf1b627db7d1dae35c6327e55
Mathew Shen <datahonor@gmail.com>
2024-08-12 20:36:35 +0800

    refactor(kmeans): centroids & clusters (#75)

--- toyml/clustering/kmeans.py
+++ toyml/clustering/kmeans.py
@@ -19,11 +19,13 @@ class Kmeans:
         >>> dataset = [[1, 0], [1, 1], [1, 2], [10, 0], [10, 1], [10, 2]]
         >>> kmeans = Kmeans(k=2).fit(dataset)
         >>> kmeans.clusters_
-        [[3, 4, 5], [0, 1, 2]]
+        {0: [0, 1, 2], 1: [3, 4, 5]}
         >>> kmeans.centroids_
-        [[10.0, 1.0], [1.0, 1.0]]
+        {0: [1.0, 1.0], 1: [10.0, 1.0]}
         >>> kmeans.predict([0, 1])
-        1
+        0
+        >>> kmeans.iter_
+        2
 
     Tip: References
         1. Zhou Zhihua
@@ -45,21 +47,10 @@ class Kmeans:
     centroids_init_method: Literal["random", "kmeans++"] = "random"
     """The method to initialize the centroids."""
     iter_: int = 0
-    clusters_: Optional[list[list[int]]] = None
+    clusters_: Optional[dict[int, list[int]]] = None
     """The clusters of the dataset."""
-    centroids_: Optional[list[list[float]]] = None
+    centroids_: Optional[dict[int, list[float]]] = None
     """The centroids of the clusters."""
-
-    def get_initial_centroids(self, dataset: list[list[float]]) -> list[list[float]]:
-        """
-        get initial centroids by a simple random selection
-        """
-        if self.centroids_init_method == "random":
-            return self._get_initial_centroids_random(dataset)
-        elif self.centroids_init_method == "kmeans++":
-            return self._get_initial_centroids_kmeans_plus(dataset)
-        else:
-            raise ValueError(f"Invalid centroids initialization method: {self.centroids_init_method}")
 
     def fit(self, dataset: list[list[float]]) -> "Kmeans":
         """
@@ -69,8 +60,8 @@ class Kmeans:
         Returns:
             self.
         """
-        centroids = self.get_initial_centroids(dataset)
-        clusters = []
+        centroids = self._get_initial_centroids(dataset)
+        clusters = dict()
         for _ in range(self.max_iter):
             self.iter_ += 1
             clusters = self._get_clusters(dataset, centroids)
@@ -98,7 +89,18 @@ class Kmeans:
             raise ValueError("The model is not fitted yet")
         return self._get_centroid_label(point, self.centroids_)
 
-    def _is_converged(self, prev_centroids: list[list[float]], centroids: list[list[float]]) -> bool:
+    def _get_initial_centroids(self, dataset: list[list[float]]) -> dict[int, list[float]]:
+        """
+        get initial centroids by a simple random selection
+        """
+        if self.centroids_init_method == "random":
+            return self._get_initial_centroids_random(dataset)
+        elif self.centroids_init_method == "kmeans++":
+            return self._get_initial_centroids_kmeans_plus(dataset)
+        else:
+            raise ValueError(f"Invalid centroids initialization method: {self.centroids_init_method}")
+
+    def _is_converged(self, prev_centroids: dict[int, list[float]], centroids: dict[int, list[float]]) -> bool:
         """
         Check if the centroids converged.
 
@@ -110,7 +112,7 @@ class Kmeans:
             Whether the centroids converged.
         """
         # check every centroid
-        for i, centroid in enumerate(centroids):
+        for i, centroid in centroids.items():
             prev_centroid = prev_centroids[i]
             # check every dimension
             for j in range(len(prev_centroid)):
@@ -118,7 +120,7 @@ class Kmeans:
                     return False
         return True
 
-    def _get_initial_centroids_random(self, dataset: list[list[float]]) -> list[list[float]]:
+    def _get_initial_centroids_random(self, dataset: list[list[float]]) -> dict[int, list[float]]:
         """
         Get initial centroids by a simple random selection.
 
@@ -128,9 +130,11 @@ class Kmeans:
         Returns:
             The initial centroids
         """
-        return random.sample(dataset, self.k)
+        data_points = random.sample(dataset, self.k)
+        centroids = {i: data_point for i, data_point in enumerate(data_points)}
+        return centroids
 
-    def _get_initial_centroids_kmeans_plus(self, dataset: list[list[float]]) -> list[list[float]]:
+    def _get_initial_centroids_kmeans_plus(self, dataset: list[list[float]]) -> dict[int, list[float]]:
         """
         Get initial centroids by k-means++ algorithm.
 
@@ -140,8 +144,7 @@ class Kmeans:
         Returns:
             The initial centroids
         """
-        # the first centroid
-        self.centroids_ = [[] for _ in range(self.k)]
+        self.centroids_ = dict()
         self.centroids_[0] = random.choice(dataset)
         for i in range(1, self.k):
             min_distances = [self._get_min_square_distance(point) for point in dataset]
@@ -162,29 +165,29 @@ class Kmeans:
         """
         assert self.centroids_ is not None
         return min(
-            (euclidean_distance(point, centroid) ** 2 for centroid in self.centroids_ if centroid), default=math.inf
+            (euclidean_distance(point, centroid) ** 2 for centroid in self.centroids_.values() if centroid),
+            default=math.inf,
         )
 
     @staticmethod
-    def _get_centroid_label(point: list[float], centroids: list[list[float]]) -> int:
+    def _get_centroid_label(point: list[float], centroids: dict[int, list[float]]) -> int:
         """
         Get the label of the centroid, which is closest to the point
         """
-        distances = [euclidean_distance(point, centroid) for centroid in centroids]
-        return distances.index(min(distances))
+        distances = [(i, euclidean_distance(point, centroid)) for i, centroid in centroids.items()]
+        return min(distances, key=lambda x: x[1])[0]
 
-    def _get_clusters(self, dataset: list[list[float]], centroids: list[list[float]]) -> list[list[int]]:
-        clusters: list[list[int]] = [[] for _ in range(self.k)]
+    def _get_clusters(self, dataset: list[list[float]], centroids: dict[int, list[float]]) -> dict[int, list[int]]:
+        clusters: dict[int, list[int]] = {i: [] for i in range(self.k)}
         for i, point in enumerate(dataset):
             centroid_label = self._get_centroid_label(point, centroids)
             clusters[centroid_label].append(i)
         return clusters
 
-    def _get_centroids(self, dataset: list[list[float]], clusters: list[list[int]]) -> list[list[float]]:
-        # clusters: indexes -> data points
-        points_clusters = [[dataset[i] for i in cluster] for cluster in clusters]
-        centroids: list[list[float]] = [[] for _ in range(self.k)]
-        for i, cluster in enumerate(points_clusters):
-            centroid = [sum(t) / len(cluster) for t in zip(*cluster)]
-            centroids[i] = centroid
+    def _get_centroids(self, dataset: list[list[float]], clusters: dict[int, list[int]]) -> dict[int, list[float]]:
+        centroids: dict[int, list[float]] = {i: [] for i in range(self.k)}
+        for cluster_i, cluster in clusters.items():
+            cluster_points = [dataset[i] for i in cluster]
+            centroid = [sum(t) / len(cluster) for t in zip(*cluster_points)]
+            centroids[cluster_i] = centroid
         return centroids
