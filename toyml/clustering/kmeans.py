65a47b5de8b47e630993d185e8a3f9f9c0be0bef
Mathew Shen <datahonor@gmail.com>
2024-08-12 21:25:02 +0800

    feat(kmeans): add fit_predict (#77)
    * feat(kmeans): add fit_predict

--- toyml/clustering/kmeans.py
+++ toyml/clustering/kmeans.py
@@ -30,6 +30,14 @@ class Kmeans:
         >>> kmeans.iter_
         2
 
+    There is a `fit_predict` method that can be used to fit and predict.
+
+    Examples:
+        >>> from toyml.clustering import Kmeans
+        >>> dataset = [[1, 0], [1, 1], [1, 2], [10, 0], [10, 1], [10, 2]]
+        >>> Kmeans(k=2).fit_predict(dataset)
+        [0, 0, 0, 1, 1, 1]
+
     Tip: References
         1. Zhou Zhihua
         2. Murphy
@@ -54,9 +62,13 @@ class Kmeans:
     """The clusters of the dataset."""
     centroids: Optional[dict[int, list[float]]] = None
     """The centroids of the clusters."""
+    labels: Optional[list[int]] = None
+    """The cluster labels of the dataset."""
 
     def fit(self, dataset: list[list[float]]) -> "Kmeans":
         """
+        Fit the dataset with K-means algorithm.
+
         Args:
             dataset: the set of data points for clustering
 
@@ -78,6 +90,19 @@ class Kmeans:
         """
         self.clusters = self._get_clusters(dataset)
         self.centroids = self._get_centroids(dataset)
+        self.labels = self._get_dataset_labels(dataset)
+
+    def fit_predict(self, dataset: list[list[float]]) -> list[int]:
+        """
+        Fit and predict the cluster label of the dataset.
+
+        Args:
+            dataset: the set of data points for clustering
+
+        Returns:
+            Cluster labels of the dataset samples.
+        """
+        return self.fit(dataset).labels  # type: ignore
 
     def predict(self, point: list[float]) -> int:
         """
@@ -93,6 +118,13 @@ class Kmeans:
         if self.centroids is None:
             raise ValueError("The model is not fitted yet")
         return self._get_centroid_label(point, self.centroids)
+
+    def _get_dataset_labels(self, dataset: list[list[float]]) -> list[int]:
+        labels = [-1] * len(dataset)
+        for cluster_label, cluster in self.clusters.items():  # type: ignore
+            for data_point_index in cluster:
+                labels[data_point_index] = cluster_label
+        return labels
 
     def _get_initial_centroids(self, dataset: list[list[float]]) -> dict[int, list[float]]:
         """
