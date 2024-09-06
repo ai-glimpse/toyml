28440bd6351437a9833a23f9623733ea126b2c67
Xiangzhuang Shen <datahonor@gmail.com>
2024-09-06 13:44:24 +0800

    refactor

--- toyml/classification/knn.py
+++ toyml/classification/knn.py
@@ -1,67 +1,115 @@
+from __future__ import annotations
+
+import math
+
 from collections import Counter
-from typing import Callable
+from dataclasses import dataclass, field
+from typing import Any, Optional
 
-from toyml.utils.linear_algebra import (
-    euclidean_distance,
-    standardization,
-    vectors_mean,
-    vectors_std,
-)
-from toyml.utils.types import DataSet, Label, Labels, Vector
+from toyml.utils.linear_algebra import euclidean_distance
 
 
-class KNeighborsClassifier:
-    """
-    The implementation of K-Nearest Neighbors.
+@dataclass
+class Standarizationer:
+    _means: list[float] = field(default_factory=list)
+    _stds: list[float] = field(default_factory=list)
 
-    Ref:
-    1. Li Hang
-    2. Tan
-    3. Zhou
-    4. Murphy
-    5. Harrington
-    """
+    def fit(self, dataset: list[list[float]]) -> Standarizationer:
+        self._means = self._vectors_mean(dataset)
+        self._stds = self._vectors_std(dataset)
+        return self
 
-    def __init__(
-        self,
-        dataset: DataSet,
-        labels: Labels,
-        k: int,
-        dist: Callable[[Vector, Vector], float] = euclidean_distance,
-        std: bool = True,
-    ) -> None:
-        if not isinstance(dataset, list):
-            raise TypeError(f"invalid type in {type(dataset)} for the 'dataset' argument")
-        self._dataset = dataset
-        self._labels = labels
-        self._k = k
-        self._dist = dist
-        # for standardization
-        self._is_std = std
-        self._means = vectors_mean(dataset)
-        self._stds = vectors_std(dataset)
+    def transform(self, dataset: list[list[float]]) -> list[list[float]]:
+        return self.standardization(dataset, self._means, self._stds)
 
-    def fit(self) -> None:
-        if self._is_std:
-            standardization(self._dataset, self._means, self._stds)
-        else:
-            pass
+    def fit_transform(self, dataset: list[list[float]]) -> list[list[float]]:
+        self.fit(dataset)
+        return self.transform(dataset)
 
-    def predict(self, x: Vector) -> Label:
-        # x -> standardization -> v
-        d = len(x)
+    def _vectors_mean(self, vectors: list[list[float]]) -> list[float]:
+        """
+        Calculate vectors mean
+
+        Example1:
+        >>> vectors_mean([[1.0, 2.0], [3.0, 4.0]])
+        [2.0, 3.0]
+        """
+        d = len(vectors[0])
+        n = len(vectors)
         v = [0.0] * d
         for i in range(d):
-            v[i] = (x[i] - self._means[i]) / self._stds[i]
-        dists = [self._dist(v, point) for point in self._dataset]
+            v[i] = sum(vector[i] for vector in vectors) / n
+        return v
+
+    def _vectors_std(self, vectors: list[list[float]]) -> list[float]:
+        """
+        Calculate vectors(every column) standard variance
+        """
+        d = len(vectors[0])
+        n = len(vectors)
+        v = [0.0] * d
+        for i in range(d):
+            v[i] = math.sqrt(sum((vector[i] - self._means[i]) ** 2 for vector in vectors) / n)
+        return v
+
+    # data transformation
+    def standardization(self, dataset: list[list[float]], means: list[float], stds: list[float]) -> list[list[float]]:
+        """
+        The standardization of numerical dataset.
+        """
+        d = len(means)
+        n = len(dataset)
+        for j in range(d):
+            for i in range(n):
+                value = dataset[i][j]
+                dataset[i][j] = (value - means[j]) / stds[j]
+        return dataset
+
+
+@dataclass
+class KNN:
+    """
+    The implementation of K-Nearest Neighbors classification algorithm.
+
+    Tip: References
+        1. Li Hang
+        2. Tan
+        3. Zhou
+        4. Murphy
+        5. Harrington
+    """
+
+    k: int
+    std_transform: bool = True
+    _dataset: Optional[list[list[float]]] = None
+    _labels: Optional[list[Any]] = None
+    _standarizationer: Optional[Standarizationer] = None
+
+    def fit(self, dataset: list[list[float]], labels: list[Any]) -> KNN:
+        self._dataset = dataset
+        self._labels = labels
+        if self.std_transform:
+            self._standarizationer = Standarizationer()
+            self._dataset = self._standarizationer.fit_transform(self._dataset)
+        return self
+
+    def predict(self, x: list[float]) -> Any:
+        if self._dataset is None or self._labels is None:
+            raise ValueError("The model is not fitted yet!")
+
+        if self.std_transform:
+            if self._standarizationer is None:
+                raise ValueError("Cannot find the standarization!")
+            x = self._standarizationer.transform([x])[0]
+        distances = [euclidean_distance(x, point) for point in self._dataset]
         # get k-nearest neighbors' label
-        labels = [x for _, x in sorted(zip(dists, self._labels))][:: self._k]
-        return Counter(labels).most_common(1)[0][0]
+        k_nearest_labels = [label for _, label in sorted(zip(distances, self._labels), key=lambda x: x[0])][:: self.k]
+        label = Counter(k_nearest_labels).most_common(1)[0][0]
+        return label
 
 
 if __name__ == "__main__":
-    dataset: DataSet = [[0.0], [1], [2], [3]]
-    labels: Labels = [0, 0, 1, 1]
-    knn = KNeighborsClassifier(dataset, labels, 3)
-    knn.fit()
+    dataset: list[list[float]] = [[0.0], [1], [2], [3]]
+    labels: list[Any] = [0, 0, 1, 1]
+    knn = KNN(3).fit(dataset, labels)
     print(knn.predict([1.1]))
