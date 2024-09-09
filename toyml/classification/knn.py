593ae6ff1a263468d355b9a6b4928da4c5b747e0
Xiangzhuang Shen <datahonor@gmail.com>
2024-09-09 21:24:52 +0800

    improve: structure and names

--- toyml/classification/knn.py
+++ toyml/classification/knn.py
@@ -1,65 +1,65 @@
 from __future__ import annotations
 
 import math
+import statistics
 
 from collections import Counter
 from dataclasses import dataclass, field
 from typing import Any, Optional
 
-from toyml.utils.linear_algebra import euclidean_distance
-
 
 @dataclass
-class Standarizationer:
+class Standardizationer:
     _means: list[float] = field(default_factory=list)
     _stds: list[float] = field(default_factory=list)
+    _dimension: Optional[int] = None
 
-    def fit(self, dataset: list[list[float]]) -> Standarizationer:
-        self._means = self._vectors_mean(dataset)
-        self._stds = self._vectors_std(dataset)
+    def fit(self, dataset: list[list[float]]) -> Standardizationer:
+        self._dimension = self._get_dataset_dimension(dataset)
+        self._means = self._dataset_column_means(dataset)
+        self._stds = self._dataset_column_stds(dataset)
         return self
 
     def transform(self, dataset: list[list[float]]) -> list[list[float]]:
-        return self.standardization(dataset, self._means, self._stds)
+        if self._dimension is None:
+            raise ValueError("The model is not fitted yet!")
+        return self.standardization(dataset)
 
     def fit_transform(self, dataset: list[list[float]]) -> list[list[float]]:
         self.fit(dataset)
         return self.transform(dataset)
 
-    def _vectors_mean(self, vectors: list[list[float]]) -> list[float]:
-        """
-        Calculate vectors mean
-        """
-        d = len(vectors[0])
-        n = len(vectors)
-        v = [0.0] * d
-        for i in range(d):
-            v[i] = sum(vector[i] for vector in vectors) / n
-        return v
-
-    def _vectors_std(self, vectors: list[list[float]]) -> list[float]:
-        """
-        Calculate vectors(every column) standard variance
-        """
-        d = len(vectors[0])
-        n = len(vectors)
-        v = [0.0] * d
-        for i in range(d):
-            v[i] = math.sqrt(sum((vector[i] - self._means[i]) ** 2 for vector in vectors) / n)
-        return v
-
-    # data transformation
-    def standardization(self, dataset: list[list[float]], means: list[float], stds: list[float]) -> list[list[float]]:
+    def standardization(self, dataset: list[list[float]]) -> list[list[float]]:
         """
         The standardization of numerical dataset.
         """
-        d = len(means)
-        n = len(dataset)
-        for j in range(d):
-            for i in range(n):
-                value = dataset[i][j]
-                dataset[i][j] = (value - means[j]) / stds[j]
+        if self._dimension is None:
+            raise ValueError("The model is not fitted yet!")
+        for j, column in enumerate(zip(*dataset)):
+            for i, value in enumerate(column):
+                dataset[i][j] = (value - self._means[j]) / self._stds[j]
         return dataset
+
+    @staticmethod
+    def _get_dataset_dimension(dataset: list[list[float]]) -> int:
+        dimension = len(dataset[0])
+        if not all([len(row) == dimension for row in dataset]):
+            raise ValueError("All rows must have the same number of columns")
+        return dimension
+
+    @staticmethod
+    def _dataset_column_means(dataset: list[list[float]]) -> list[float]:
+        """
+        Calculate vectors mean
+        """
+        return [statistics.mean(column) for column in zip(*dataset)]
+
+    @staticmethod
+    def _dataset_column_stds(dataset: list[list[float]]) -> list[float]:
+        """
+        Calculate vectors(every column) standard variance
+        """
+        return [statistics.stdev(column) for column in zip(*dataset)]
 
 
 @dataclass
@@ -77,35 +77,44 @@ class KNN:
 
     k: int
     std_transform: bool = True
-    _dataset: Optional[list[list[float]]] = None
-    _labels: Optional[list[Any]] = None
-    _standarizationer: Optional[Standarizationer] = None
+    dataset_: Optional[list[list[float]]] = None
+    labels_: Optional[list[Any]] = None
+    standarizationer_: Optional[Standardizationer] = None
 
     def fit(self, dataset: list[list[float]], labels: list[Any]) -> KNN:
-        self._dataset = dataset
-        self._labels = labels
+        self.dataset_ = dataset
+        self.labels_ = labels
         if self.std_transform:
-            self._standarizationer = Standarizationer()
-            self._dataset = self._standarizationer.fit_transform(self._dataset)
+            self.standarizationer_ = Standardizationer()
+            self.dataset_ = self.standarizationer_.fit_transform(self.dataset_)
         return self
 
     def predict(self, x: list[float]) -> Any:
-        if self._dataset is None or self._labels is None:
+        if self.dataset_ is None or self.labels_ is None:
             raise ValueError("The model is not fitted yet!")
 
         if self.std_transform:
-            if self._standarizationer is None:
-                raise ValueError("Cannot find the standarization!")
-            x = self._standarizationer.transform([x])[0]
-        distances = [euclidean_distance(x, point) for point in self._dataset]
+            if self.standarizationer_ is None:
+                raise ValueError("Cannot find the standardization!")
+            x = self.standarizationer_.transform([x])[0]
+        distances = [self._calculate_distance(x, point) for point in self.dataset_]
         # get k-nearest neighbors' label
-        k_nearest_labels = [label for _, label in sorted(zip(distances, self._labels), key=lambda x: x[0])][:: self.k]
+        k_nearest_labels = [label for _, label in sorted(zip(distances, self.labels_), key=lambda x: x[0])][:: self.k]
         label = Counter(k_nearest_labels).most_common(1)[0][0]
         return label
+
+    @staticmethod
+    def _calculate_distance(x: list[float], y: list[float]) -> float:
+        """
+        TODO: Implement other distance metrics
+        """
+        assert len(x) == len(y), f"{x} and {y} have different length!"
+        return math.sqrt(sum(pow(x[i] - y[i], 2) for i in range(len(x))))
 
 
 if __name__ == "__main__":
     dataset: list[list[float]] = [[0.0], [1], [2], [3]]
     labels: list[Any] = [0, 0, 1, 1]
     knn = KNN(3).fit(dataset, labels)
+    print(knn.labels_)
     print(knn.predict([1.1]))
