5ea74d3fdbe8c674a59494d129e3b348beff66e0
Xiangzhuang Shen <datahonor@gmail.com>
2024-11-05 13:13:00 +0800

    improve: mean, var calculation

--- toyml/classification/naive_bayes.py
+++ toyml/classification/naive_bayes.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import math
+import statistics
 
 from dataclasses import dataclass, field
 
@@ -15,7 +16,7 @@ class GaussianNaiveBayes:
     """The prior probability of each class in training dataset"""
     means_: dict[int, list[float]] = field(default_factory=dict)
     """The means of each class in training dataset"""
-    vars_: dict[int, list[float]] = field(default_factory=dict)
+    variances_: dict[int, list[float]] = field(default_factory=dict)
     """The variance of each class in training dataset"""
 
     def fit(self, dataset: list[list[float]], labels: list[int]) -> GaussianNaiveBayes:
@@ -23,8 +24,7 @@ class GaussianNaiveBayes:
         self.labels_ = sorted(set(labels))
         self.class_count_ = len(set(labels))
         self.class_prior_ = {label: 1 / self.class_count_ for label in self.labels_}
-        self.means_ = self._get_classes_means(dataset, labels)
-        self.vars_ = self._get_classes_vars(dataset, labels)
+        self.means_, self.variances_ = self._get_classes_means_variances(dataset, labels)
         return self
 
     def predict(self, sample: list[float]) -> int:
@@ -32,6 +32,7 @@ class GaussianNaiveBayes:
         raw_label_posteriors: dict[int, float] = {}
         for label, likelihood in label_likelihoods.items():
             raw_label_posteriors[label] = likelihood * self.class_prior_[label]
+        print(raw_label_posteriors)
         evidence = sum(raw_label_posteriors.values())
         label_posteriors = {label: raw_posterior / evidence for label, raw_posterior in raw_label_posteriors.items()}
         label = max(label_posteriors, key=lambda k: label_posteriors[k])
@@ -44,7 +45,7 @@ class GaussianNaiveBayes:
         label_likelihoods: dict[int, float] = {}
         for label in self.labels_:
             label_means = self.means_[label]
-            label_vars = self.vars_[label]
+            label_vars = self.variances_[label]
             likelihood = 1.0
             for i, xi in enumerate(sample):
                 # TODO: try to calculate the log-likelihood
@@ -54,20 +55,31 @@ class GaussianNaiveBayes:
             label_likelihoods[label] = likelihood
         return label_likelihoods
 
-    def _get_classes_means(self, dataset: list[list[float]], labels: list[int]) -> dict[int, list[float]]:
-        dimension_num = len(dataset[0])
+    def _get_classes_means_variances(
+        self,
+        dataset: list[list[float]],
+        labels: list[int],
+    ) -> tuple[dict[int, list[float]], dict[int, list[float]]]:
+        means, variances = {}, {}
+        for label in self.labels_:
+            label_samples = [sample for (sample, sample_label) in zip(dataset, labels) if sample_label == label]
+            means[label] = self._dataset_column_means(label_samples)
+            variances[label] = self._dataset_column_variances(label_samples)
+        return means, variances
 
-        label_count = {label: 0 for label in self.labels_}
-        label_dimension_sums = {label: [0.0] * dimension_num for label in self.labels_}
-        for label, sample in zip(labels, dataset):
-            label_count[label] += 1
-            for dim, xi in enumerate(sample):
-                label_dimension_sums[label][dim] += xi
-        means = {
-            label: [dimension_sum / label_count[label] for dimension_sum in dimension_sums]
-            for label, dimension_sums in label_dimension_sums.items()
-        }
-        return means
+    @staticmethod
+    def _dataset_column_means(dataset: list[list[float]]) -> list[float]:
+        """
+        Calculate vectors mean
+        """
+        return [statistics.mean(column) for column in zip(*dataset, strict=True)]
+
+    @staticmethod
+    def _dataset_column_variances(dataset: list[list[float]]) -> list[float]:
+        """
+        Calculate vectors(every column) standard variance
+        """
+        return [statistics.variance(column) for column in zip(*dataset, strict=True)]
 
     def _get_classes_vars(self, dataset: list[list[float]], labels: list[int]) -> dict[int, list[float]]:
         dimension_num = len(dataset[0])
