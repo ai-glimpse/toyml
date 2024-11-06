771c9033d9a97cb5371def4352353a8eadfbaddb
Xiangzhuang Shen <datahonor@gmail.com>
2024-11-06 16:34:45 +0800

    improve: log likelihood of gaussian naive bayes

--- toyml/classification/naive_bayes.py
+++ toyml/classification/naive_bayes.py
@@ -41,16 +41,29 @@ class GaussianNaiveBayes:
         return self
 
     def predict(self, sample: list[float]) -> int:
-        label_likelihoods = self._likelihood(sample)
-        raw_label_posteriors: dict[int, float] = {}
-        for label, likelihood in label_likelihoods.items():
-            raw_label_posteriors[label] = likelihood * self.class_prior_[label]
-        evidence = sum(raw_label_posteriors.values())
-        label_posteriors = {label: raw_posterior / evidence for label, raw_posterior in raw_label_posteriors.items()}
+        label_posteriors = self.predict_prob(sample)
         label = max(label_posteriors, key=lambda k: label_posteriors[k])
         return label
 
-    def _likelihood(self, sample: list[float]) -> dict[int, float]:
+    def predict_prob(self, sample: list[float], normalization: bool = True) -> dict[int, float]:
+        label_posteriors = self.predict_log_prob(sample, normalization)
+        return {label: math.exp(log_prob) for label, log_prob in label_posteriors.items()}
+
+    def predict_log_prob(self, sample: list[float], normalization: bool = True) -> dict[int, float]:
+        label_likelihoods = self._log_likelihood(sample)
+        raw_label_posteriors: dict[int, float] = {}
+        for label, likelihood in label_likelihoods.items():
+            raw_label_posteriors[label] = likelihood + math.log(self.class_prior_[label])
+        if normalization is False:
+            return raw_label_posteriors
+        # ref: https://github.com/scikit-learn/scikit-learn/blob/2beed55847ee70d363bdbfe14ee4401438fba057/sklearn/naive_bayes.py#L97
+        logsumexp_prob = math.log(sum(math.exp(log_prob) for log_prob in raw_label_posteriors.values()))
+        label_posteriors = {
+            label: raw_posterior - logsumexp_prob for label, raw_posterior in raw_label_posteriors.items()
+        }
+        return label_posteriors
+
+    def _log_likelihood(self, sample: list[float]) -> dict[int, float]:
         """
         Calculate the likelihood of each sample in each class
         """
@@ -58,13 +71,13 @@ class GaussianNaiveBayes:
         for label in self.labels_:
             label_means = self.means_[label]
             label_vars = self.variances_[label]
-            likelihood = 1.0
+            log_likelihood = 0.0
             for i, xi in enumerate(sample):
-                # TODO: try to calculate the log-likelihood
-                likelihood *= (1 / math.sqrt(2 * math.pi * label_vars[i])) * math.exp(
-                    -((xi - label_means[i]) ** 2) / (2 * label_vars[i])
+                # calculate the log-likelihood
+                log_likelihood += -0.5 * math.log(2 * math.pi * label_vars[i]) - (
+                    (xi - label_means[i]) ** 2 / (2 * label_vars[i])
                 )
-            label_likelihoods[label] = likelihood
+            label_likelihoods[label] = log_likelihood
         return label_likelihoods
 
     def _get_classes_means_variances(
@@ -193,22 +206,39 @@ class MultinomialNaiveBayes:
 
 
 if __name__ == "__main__":
-    import numpy as np
+    # GaussianNB
+    label = [0, 0, 0, 0, 1, 1, 1, 1]
+    dataset = [
+        [6.00, 180, 12],
+        [5.92, 190, 11],
+        [5.58, 170, 12],
+        [5.92, 165, 10],
+        [5.00, 100, 6],
+        [5.50, 150, 8],
+        [5.42, 130, 7],
+        [5.75, 150, 9],
+    ]
+    clf = GaussianNaiveBayes().fit(dataset, label)
+    print(clf.predict([6.00, 130, 8]))
+    print(clf.predict_prob([6.00, 130, 8], normalization=False))
 
-    rng = np.random.RandomState(1)
-    X = rng.randint(5, size=(6, 10))
-    y = np.array([1, 2, 3, 4, 5, 6])
-    from sklearn.naive_bayes import MultinomialNB
-
-    clf = MultinomialNB()
-    clf.fit(X, y)
-    print(clf.predict(X[2:3]))
-    print(clf.predict_proba(X[2:3]))
-    print(clf.predict_log_proba(X[2:3]))
-
-    clf1 = MultinomialNaiveBayes(alpha=1)
-    clf1.fit([[int(v) for v in s] for s in X], [int(v) for v in y])
-    sample = [int(x) for x in X[2:3][0]]
-    print(clf1.predict(sample))
-    print(clf1.predict_prob(sample))
-    print(clf1.predict_log_prob(sample))
+    # MultinomialNB
+    # import numpy as np
+    #
+    # rng = np.random.RandomState(1)
+    # X = rng.randint(5, size=(6, 10))
+    # y = np.array([1, 2, 3, 4, 5, 6])
+    # from sklearn.naive_bayes import MultinomialNB
+    #
+    # clf = MultinomialNB()
+    # clf.fit(X, y)
+    # print(clf.predict(X[2:3]))
+    # print(clf.predict_proba(X[2:3]))
+    # print(clf.predict_log_proba(X[2:3]))
+    #
+    # clf1 = MultinomialNaiveBayes(alpha=1)
+    # clf1.fit([[int(v) for v in s] for s in X], [int(v) for v in y])
+    # sample = [int(x) for x in X[2:3][0]]
+    # print(clf1.predict(sample))
+    # print(clf1.predict_prob(sample))
+    # print(clf1.predict_log_prob(sample))
