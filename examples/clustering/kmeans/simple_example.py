43f696547540c14c81791f120308aa85476162a9
Mathew Shen <datahonor@gmail.com>
2026-01-06 14:17:53 +0800

    chore: add examples and scripts (#189)
    * chore: add examples and scripts

---
+++ examples/clustering/kmeans/simple_example.py
@@ -0,0 +1,17 @@
+import math
+
+# data points
+points = [
+    [1, 1],
+    [1, 2],
+    [5, 3],
+    [6, 4],
+]
+
+
+# euclidean distance
+def euclidean(
+    x: list[float],
+    y: list[float],
+) -> float:
+    return math.sqrt(sum(pow(x[i] - y[i], 2) for i in range(len(x))))
